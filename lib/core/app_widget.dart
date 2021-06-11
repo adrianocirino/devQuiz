e768d9f47203959d1116d669ee32b7c60a12a9c6
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-06-11 14:59:17 -0300

    Estrutura inicial

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,14 @@
+import 'package:dev_quiz/splash/splas_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({ Key? key }) : super(key: key);
+  const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: Scaffold()
-    );
+      home: SplashPage(),
+    );        
   }
 }
