c1b208f4e04ea8857b402730724b458842eb38b0
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-06-15 23:35:47 -0300

    Criado homeRepository

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dev_quiz/core/app_images.dart';
+import 'package:dev_quiz/home/home_repository.dart';
 import 'package:dev_quiz/home/home_state.dart';
 import 'package:dev_quiz/shared/models/awnser_model.dart';
 import 'package:dev_quiz/shared/models/question_model.dart';
@@ -14,50 +15,19 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
 
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-  
-    user = UserModel(
-      name: "Adriano Cirino da Silva",
-      photoUrl: "https://avatars.githubusercontent.com/u/19496139?v=4",
-    );
+    user = await repository.getUser();
     state = HomeState.sucess;
 
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 Flutter",
-        imagem: AppImages.blocks,
-        level: Level.facil,
-        questionAnswered: 1,
-        questions: [
-          QuestionModel(
-            title: "Está curtindo o flutter ?",
-            awnsers: [
-              AwnserModel(title: "Estou curtindo"),
-              AwnserModel(title: "Estou adorando"),
-              AwnserModel(title: "Amando flutter"),
-              AwnserModel(title: "Show de bola!", isRight: true),
-            ]
-          ),
-          QuestionModel(
-            title: "Está curtindo o flutter ?",
-            awnsers: [
-              AwnserModel(title: "Estou curtindo"),
-              AwnserModel(title: "Estou adorando"),
-              AwnserModel(title: "Amando flutter"),
-              AwnserModel(title: "Show de bola!", isRight: true),
-            ]
-          ),
-        ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.sucess;
   }
 }
--- lib/shared/models/question_model.dart
+++ lib/shared/models/question_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dev_quiz/shared/models/awnser_model.dart';
 
 class QuestionModel {
@@ -5,4 +7,22 @@ class QuestionModel {
   final List<AwnserModel> awnsers;
 
   QuestionModel({ required this.title, required this.awnsers}) : assert(awnsers.length == 4);
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'awnsers': awnsers.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory QuestionModel.fromMap(Map<String, dynamic> map) {
+    return QuestionModel(
+      title: map['title'],
+      awnsers: List<AwnserModel>.from(map['awnsers']?.map((x) => AwnserModel.fromMap(x))),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuestionModel.fromJson(String source) => QuestionModel.fromMap(json.decode(source));
 }
