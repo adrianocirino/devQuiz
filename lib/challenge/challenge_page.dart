fe37d9fd7b969548454e3fda2982a3a633225345
adrianocirino <adriano.cirino.da.silva@gmail.com>
2021-06-22 21:25:48 -0300

    Criado nextButtonWidget

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,9 +1,10 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatefulWidget {
-  const ChallengePage({ Key? key }) : super(key: key);
+  const ChallengePage({Key? key}) : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
@@ -14,13 +15,25 @@ class _ChallengePageState extends State<ChallengePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80),
-        child: SafeArea(
-          top: true,
-          child: QuestionIndicatorWidget()
-        )
+          preferredSize: Size.fromHeight(80),
+          child: SafeArea(top: true, child: QuestionIndicatorWidget())),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade ?",
       ),
-      body: QuizWidget(title: "O que o Flutter faz em sua totalidade ?",),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Pular", onTap: () {},)),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar", onTap: () {})),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
